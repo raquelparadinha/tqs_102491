ddbd7e015cf59132283f1c218bb1f0b4fa6081d9
Raquel Paradinha <raquelparadinha@gmail.com>
2023-02-17 13:00:10 +0000

    class 1 exercises

---
+++ lab1/lab1_1_stack/src/main/java/tqs/lab1/TqsStack.java
@@ -0,0 +1,27 @@
+package tqs.lab1;
+
+import java.util.*;
+
+public class TqsStack <T>{
+    private LinkedList<T> stack;
+
+    public TqsStack() {}
+
+    public T pop() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public void push(T t) {}
+
+    public boolean isEmpty() {
+        return true;
+    }
+}
